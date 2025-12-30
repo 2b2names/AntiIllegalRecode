1a385404bc4c909a44f29335be76a28ab8e05f08
2b2t_names <90040707+2b2names@users.noreply.github.com>
2025-12-30 15:39:39 +0300

    Update ActionHandler.java

--- src/main/java/me/tiger/antiIllegalRecode/action/ActionHandler.java
+++ src/main/java/me/tiger/antiIllegalRecode/action/ActionHandler.java
@@ -1,42 +1,15 @@
 package me.tiger.antiIllegalRecode.action;
 
-import me.tiger.antiIllegalRecode.AntiIllegalRecode;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.stream.Collectors;
 
 public class ActionHandler {
 
     public static void handle(Player player, ItemStack item) {
-        if (item == null || item.getType().isAir()) return; // ignore empty slots
-
-        // Remove illegal item
         player.getInventory().remove(item);
-        player.sendMessage("§cIllegal item detected and removed!");
-
-        // Build enchantment string if any
-        String enchants = "";
-        if (!item.getEnchantments().isEmpty()) {
-            enchants = item.getEnchantments().entrySet().stream()
-                    .map(e -> e.getKey().getKey().getKey() + ":" + e.getValue())
-                    .collect(Collectors.joining(", "));
-        }
-
-        // Build coordinates string
-        String coords = "X: " + player.getLocation().getBlockX() +
-                " Y: " + player.getLocation().getBlockY() +
-                " Z: " + player.getLocation().getBlockZ();
-
-        // Send webhook
-        AntiIllegalRecode.getInstance().getWebhook().sendMessage(
-                "Player: " + player.getName() +
-                        " | Item: " + item.getType() +
-                        " | Amount: " + item.getAmount() +
-                        (enchants.isEmpty() ? "" : " | Enchants: " + enchants) +
-                        " | Coords: " + coords
-        );
-
-        System.out.println("[AntiIllegalRecode] Handled illegal item from " + player.getName());
+        String msg = "Illegal item removed from " + player.getName() + ": " + item.getType();
+        player.sendMessage("§c" + msg);
+        Bukkit.getConsoleSender().sendMessage(msg);
     }
 }
