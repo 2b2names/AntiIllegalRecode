a92ad8b817504a38b5d12853a81a8ee667172c34
2b2t_names <90040707+2b2names@users.noreply.github.com>
2025-12-30 15:41:06 +0300

    Update InventoryListener.java

--- src/main/java/me/tiger/antiIllegalRecode/listener/InventoryListener.java
+++ src/main/java/me/tiger/antiIllegalRecode/listener/InventoryListener.java
@@ -2,7 +2,10 @@ package me.tiger.antiIllegalRecode.listener;
 
 import me.tiger.antiIllegalRecode.AntiIllegalRecode;
 import me.tiger.antiIllegalRecode.action.ActionHandler;
+import me.tiger.antiIllegalRecode.check.BlockCheck;
+import me.tiger.antiIllegalRecode.check.EnchantCheck;
 import me.tiger.antiIllegalRecode.check.ItemCheck;
+import me.tiger.antiIllegalRecode.check.StackCheck;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,19 +16,24 @@ public class InventoryListener implements Listener {
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        // Plugin must be enabled
         if (!AntiIllegalRecode.getInstance().isPluginEnabled()) return;
 
-        // Only handle player clicks
         if (!(event.getWhoClicked() instanceof Player player)) return;
+        if (player.isOp()) return;
 
         ItemStack item = event.getCurrentItem();
-        // Ignore empty slots
         if (item == null || item.getType().isAir()) return;
 
-        // Check if the item is illegal
-        if (ItemCheck.isIllegal(item)) {
+        boolean illegal = false;
+
+        if (ItemCheck.isIllegal(item)) illegal = true;
+        if (BlockCheck.isIllegal(item)) illegal = true;
+        if (EnchantCheck.isIllegal(item)) illegal = true;
+        if (StackCheck.isIllegal(item)) illegal = true;
+
+        if (illegal) {
             ActionHandler.handle(player, item);
         }
     }
 }
+
